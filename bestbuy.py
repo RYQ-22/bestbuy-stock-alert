d969364204b62ad2b17c9d2618a95a268024d59c
Ricky <ryq200404@gmail.com>
2025-06-08 00:32:14 -0700

    Add provider gmail

--- bestbuy.py
+++ bestbuy.py
@@ -5,15 +5,22 @@ import time
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
+import smtplib
+from email.message import EmailMessage
 
 load_dotenv("bestbuy.env")
 
+# Environment variables
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 MESSAGING_SERVICE_SID = os.getenv("MESSAGING_SERVICE_SID")
-RECIPIENT_PHONE_NUMBER = os.getenv("RECIPIENT_PHONE_NUMBER") 
+RECIPIENT_PHONE_NUMBER = os.getenv("RECIPIENT_PHONE_NUMBER")
+GOOGLE_APPLICATION_PASSWORD = os.getenv("GOOGLE_APPLICATION_PASSWORD")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
 
 DEFAULT_URL = "https://www.bestbuy.com/site/nvidia-geforce-rtx-5080-16gb-gddr7-graphics-card-gun-metal/6614153.p" # Default: 5080 fe
+DEFAULT_PROVIDER = "twilio"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
@@ -75,7 +82,6 @@ def check_button_status(target_url):
             elif found_test_id == "in-store-only":
                 found_state = "In Store Only"
             else:
-                button_text_lower = button_text.lower()
                 found_state = f"Unknown: {button_text}"
 
         return found_state
@@ -89,6 +95,32 @@ def check_button_status(target_url):
     except Exception as e:
         return f"Error: Unknown Error: {e}"
 
+def send_msg(msg_title, msg_body):
+    if provider == "twilio":
+        send_msg_via_twilio(msg_title, msg_body)
+    elif provider == "gmail":
+        send_msg_via_gmail(msg_title, msg_body)
+
+def send_msg_via_twilio(msg_title, msg_body):
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    client.messages.create(
+        messaging_service_sid=MESSAGING_SERVICE_SID,
+        body=msg_title+"\n"+msg_body,
+        to=RECIPIENT_PHONE_NUMBER
+    )
+
+def send_msg_via_gmail(msg_title, msg_body):
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(SENDER_EMAIL, GOOGLE_APPLICATION_PASSWORD)
+    msg = EmailMessage()
+    msg['From'] = SENDER_EMAIL
+    msg['To'] = RECIPIENT_EMAIL
+    msg['Subject'] = msg_title
+    msg.set_content(msg_body)
+    s.send_message(msg)
+    s.quit()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Check Best Buy product availability.")
     parser.add_argument(
@@ -96,10 +128,18 @@ if __name__ == "__main__":
         type=str,
         nargs='?',
         default=DEFAULT_URL,
-        help="The URL of the product page to check (optional, defaults to DEFAULT_URL)."
+        help="The URL of the product page to check (optional, defaults to 5080FE)."
+    )
+    parser.add_argument(
+        "--provider",
+        type=str,
+        choices=["twilio", "gmail"],
+        default=DEFAULT_PROVIDER,
+        help="The service provider to send messages (optional, defaults to twilio)"
     )
     args = parser.parse_args()
     url = args.url
+    provider = args.provider
 
     print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Checking status for: {url} ...")
 
@@ -109,14 +149,10 @@ if __name__ == "__main__":
         print("Result: Sold Out\n")       # Sold Out
     elif status == "Add to Cart":
         print("Result: Add to Cart\n")    # Add to Cart
-        client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
         product_name = url.split('/')[-2].replace('-', ' ').title() # Get product name from url
-        sms_body = f"Best Buy Stock Alert!\nProduct: {product_name}\nStatus: Add to Cart!\nLink: {url}"
-        message = client.messages.create(
-            messaging_service_sid=MESSAGING_SERVICE_SID,
-            body=sms_body,
-            to=RECIPIENT_PHONE_NUMBER
-        )
+        msg_title = f"Best Buy Stock Alert!"
+        msg_body = f"Product: {product_name}\nStatus: Add to Cart!\nLink: {url}"
+        send_msg(msg_title, msg_body, provider)
     elif "Error" in status:
         print(f"{status}\n")              # Error
     else:
